050bee7718afc73a2bbf3f9d4cbcd8ac99d185eb
Schansge <grosser.knuff@googlemail.com>
2012-01-11 14:40:00 +0100

    TreeView improved

--- src/windows/mainwindow.py
+++ src/windows/mainwindow.py
@@ -33,18 +33,32 @@ class MainWindow(QtGui.QMainWindow):
         btn.resize(btn.sizeHint())
 
         # create treeview
+        treeGrid = QtGui.QGridLayout()
+        treeLabel = QtGui.QLabel('Layer:')
+
         treeWidget = QtGui.QTreeWidget()
         treeWidget.setColumnCount(2)
-        items = []
-        for i in range(10):
-            items.append(QtGui.QTreeWidgetItem(None, ['item: {}'.format(i), 'bla']))
-        treeWidget.addTopLevelItems(items) 
+        treeLabel.setBuddy(treeWidget)
+
+        topItems = []
+        topItems.append(QtGui.QTreeWidgetItem(None, ['Electric']))
+        topItems.append(QtGui.QTreeWidgetItem(None, ['Magnetic']))
+        topItems.append(QtGui.QTreeWidgetItem(None, ['Source']))
+        treeWidget.addTopLevelItems(topItems)
         
+        QtGui.QTreeWidgetItem(topItems[-1], ['Radar 1', 'sin(2.0*pi*x)'])
+        QtGui.QTreeWidgetItem(topItems[0], ['Kupfer 1', 'blupp'])
+        QtGui.QTreeWidgetItem(topItems[0], ['Kupfer 2', 'bla'])
+        QtGui.QTreeWidgetItem(topItems[1], ['Magnet', 'kreis'])
+
+        treeGrid.addWidget(treeLabel, 0, 0)
+        treeGrid.addWidget(treeWidget, 1, 0)
+
         # layout
         grid = QtGui.QGridLayout()
         grid.addWidget(self.canvas, 0, 0)
         grid.addWidget(btn, 1, 0)
-        grid.addWidget(treeWidget, 0, 1)
+        grid.addLayout(treeGrid, 0, 1)
         self.container.setLayout(grid)
 
     def create_actions(self):
