ebf4f1e8577aeb74912112e992247968633760df
Schansge <grosser.knuff@googlemail.com>
2012-01-23 15:13:36 +0100

    pep8 conform

--- src/plugins/functionparser.py
+++ src/plugins/functionparser.py
@@ -2,6 +2,7 @@ from pyfdtd import *
 from numpy import *
 from math import *
 
+
 def mask_from_string(expression):
     def res(x, y):
         if eval(expression):
@@ -9,7 +10,8 @@ def mask_from_string(expression):
         return 0.0
     return res
 
+
 def source_from_string(expression):
     def res(flux, deltaT, t, mem):
-        return -0.5*deltaT*eval(expression)
+        return -0.5 * deltaT * eval(expression)
     return res
