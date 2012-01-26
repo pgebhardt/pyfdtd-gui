6b416396ba36d30ff468bd5c48a69b57e94304c8
Schansge <grosser.knuff@googlemail.com>
2012-01-26 21:39:02 +0100

    started porting to qml, but does not work right now

--- src/main.py
+++ src/main.py
@@ -1,12 +1,28 @@
 import sys
-from PySide import QtCore, QtGui
+from PySide.QtGui import *
+from PySide.QtCore import *
+from PySide.QtDeclarative import *
 import windows
-import dialogs
 
-# create application
-app = QtGui.QApplication(sys.argv)
+# create qt application
+app = QApplication(sys.argv)
 
-mainWindow = windows.MainWindow()
-mainWindow.show()
+# create view
+view = QDeclarativeView()
 
+# create url to qml file
+url = QUrl('./windows/MainWindow.qml')
+
+# register plot
+qmlRegisterType(windows.Plot, 'MatplotLib', 1, 0, 'Plot')
+# set qml file
+view.setSource(url)
+
+# connect engine quit to application quit
+view.engine().quit.connect(app.quit)
+
+# show view
+view.show()
+
+# enter qt main loop
 sys.exit(app.exec_())
