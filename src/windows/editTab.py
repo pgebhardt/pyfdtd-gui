c9207030fc0a616723c32042b83d464be9bdb240
Schansge <grosser.knuff@googlemail.com>
2012-02-10 14:45:12 +0100

    duration edit tested and workin, closes #7

--- src/windows/editTab.py
+++ src/windows/editTab.py
@@ -25,101 +25,30 @@ class EditTab(QtGui.QWidget):
         # create simulation buttons
         simulationButtonBox = QtGui.QHBoxLayout()
 
-        durationEdit = QtGui.QLineEdit(
+        self.durationEdit = QtGui.QLineEdit(
                 '{}'.format(self.mainwindow.job.config['duration']))
+        self.durationEdit.textChanged.connect(self.duration_update)
+
         durationLabel = QtGui.QLabel('Duration:')
-        durationLabel.setBuddy(durationEdit)
+        durationLabel.setBuddy(self.durationEdit)
 
         startSimButton = QtGui.QPushButton('Start Simulation')
         startSimButton.clicked.connect(self.mainwindow.run_simulation)
 
         simulationButtonBox.addWidget(durationLabel)
-        simulationButtonBox.addWidget(durationEdit)
+        simulationButtonBox.addWidget(self.durationEdit)
         simulationButtonBox.addWidget(startSimButton)
-
-        # button functions
-        def new_layer_clicked():
-            self.newLayerDialog = dialogs.NewLayer(mainwindow=self.mainwindow)
-            self.newLayerDialog.okButton.clicked.connect(new_layer_create)
-            self.newLayerDialog.show()
-
-        def new_layer_create():
-            # cloase dialof
-            self.newLayerDialog.close()
-
-            # get attributes
-            name = self.newLayerDialog.nameEdit.text()
-            type_ = self.newLayerDialog.typeComboBox.currentText()
-            mask = self.newLayerDialog.maskEdit.text()
-            function = self.newLayerDialog.functionEdit.text()
-            x = float(self.newLayerDialog.xEdit.text())
-            y = float(self.newLayerDialog.yEdit.text())
-
-            # create new Layer
-            self.new_layer(name, type_, mask, function=function, x=x, y=y)
-
-            # add to job
-            if type_ == 'Electric':
-                self.mainwindow.job.material['electric'].append(
-                        [name, mask, function])
-
-            elif type_ == 'Magnetic':
-                self.mainwindow.job.material['magnetic'].append(
-                        [name, mask, function])
-
-            elif type_ == 'Source':
-                self.mainwindow.job.source.append([name, mask, function])
-
-            elif type_ == 'Listener':
-                self.mainwindow.job.listener.append([name, x, y])
-
-            # update plot
-            self.plot.update()
-            self.mainwindow.playTab.update()
-
-        def deleate_layer():
-            # get current layer
-            currentItem = self.treeWidget.currentItem()
-
-            # get parent
-            if not currentItem is None:
-                parent = currentItem.parent()
-
-            # delete layer from tree and job
-            if not currentItem is None and not parent is None:
-                # delete layer from job
-                index = parent.indexOfChild(currentItem)
-                type_ = parent.text(0)
-
-                if type_ == 'Electric':
-                    self.mainwindow.job.material['electric'].remove(
-                            self.mainwindow.job.material['electric'][index])
-                elif type_ == 'Magnetic':
-                    self.mainwindow.job.material['magnetic'].remove(
-                            self.mainwindow.job.material['magnetic'][index])
-                elif type_ == 'Source':
-                    self.mainwindow.job.source.remove(
-                            self.mainwindow.job.source[index])
-                elif type_ == 'Listener':
-                    self.mainwindow.job.listener.remove(
-                            self.mainwindow.job.listener[index])
-
-                # delete layer from tree
-                parent.removeChild(currentItem)
-
-                # update plot
-                self.plot.update()
 
         # create button grid
         layerButtonGrid = QtGui.QGridLayout()
 
         # create new layer button
         newLayerButton = QtGui.QPushButton('New Layer')
-        newLayerButton.clicked.connect(new_layer_clicked)
+        newLayerButton.clicked.connect(self.new_layer_clicked)
 
         # create delete layer buttons
         deleteLayerButton = QtGui.QPushButton('Delete Layer')
-        deleteLayerButton.clicked.connect(deleate_layer)
+        deleteLayerButton.clicked.connect(self.deleate_layer)
 
         # add buttons to grid layout
         layerButtonGrid.addWidget(newLayerButton, 0, 0)
@@ -156,6 +85,84 @@ class EditTab(QtGui.QWidget):
         self.layerItems.append(QtGui.QTreeWidgetItem(None, ['Source']))
         self.layerItems.append(QtGui.QTreeWidgetItem(None, ['Listener']))
         self.treeWidget.addTopLevelItems(self.layerItems)
+
+    # Button methods
+    def duration_update(self):
+        # update duration
+        self.mainwindow.job.config['duration'] = \
+        float(self.durationEdit.text())
+
+    def new_layer_clicked(self):
+        self.newLayerDialog = dialogs.NewLayer(mainwindow=self.mainwindow)
+        self.newLayerDialog.okButton.clicked.connect(self.new_layer_create)
+        self.newLayerDialog.show()
+
+    def new_layer_create(self):
+        # cloase dialog
+        self.newLayerDialog.close()
+
+        # get attributes
+        name = self.newLayerDialog.nameEdit.text()
+        type_ = self.newLayerDialog.typeComboBox.currentText()
+        mask = self.newLayerDialog.maskEdit.text()
+        function = self.newLayerDialog.functionEdit.text()
+        x = float(self.newLayerDialog.xEdit.text())
+        y = float(self.newLayerDialog.yEdit.text())
+
+        # create new Layer
+        self.new_layer(name, type_, mask, function=function, x=x, y=y)
+
+        # add to job
+        if type_ == 'Electric':
+            self.mainwindow.job.material['electric'].append(
+                    [name, mask, function])
+
+        elif type_ == 'Magnetic':
+            self.mainwindow.job.material['magnetic'].append(
+                    [name, mask, function])
+
+        elif type_ == 'Source':
+            self.mainwindow.job.source.append([name, mask, function])
+
+        elif type_ == 'Listener':
+            self.mainwindow.job.listener.append([name, x, y])
+
+        # update plot
+        self.plot.update()
+        self.mainwindow.playTab.update()
+
+    def deleate_layer(self):
+        # get current layer
+        currentItem = self.treeWidget.currentItem()
+
+        # get parent
+        if not currentItem is None:
+            parent = currentItem.parent()
+
+        # delete layer from tree and job
+        if not currentItem is None and not parent is None:
+            # delete layer from job
+            index = parent.indexOfChild(currentItem)
+            type_ = parent.text(0)
+
+            if type_ == 'Electric':
+                self.mainwindow.job.material['electric'].remove(
+                        self.mainwindow.job.material['electric'][index])
+            elif type_ == 'Magnetic':
+                self.mainwindow.job.material['magnetic'].remove(
+                        self.mainwindow.job.material['magnetic'][index])
+            elif type_ == 'Source':
+                self.mainwindow.job.source.remove(
+                        self.mainwindow.job.source[index])
+            elif type_ == 'Listener':
+                self.mainwindow.job.listener.remove(
+                        self.mainwindow.job.listener[index])
+
+            # delete layer from tree
+            parent.removeChild(currentItem)
+
+            # update plot
+            self.plot.update()
 
     def update(self):
         # clear editTab tree
